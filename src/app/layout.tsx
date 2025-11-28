6e51fe2674eaa56f9ca010e9619fd243ae3ececc
oulla898 <your-email@example.com>
2025-11-28 10:02:15 +0400

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ClerkProvider } from '@clerk/nextjs';
 import Script from "next/script";
 import "./globals.css";
@@ -53,6 +54,7 @@ export default function RootLayout({
         >
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
